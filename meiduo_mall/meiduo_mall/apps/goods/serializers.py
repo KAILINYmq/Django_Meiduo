a1f1ebcc72983dd1a03aef0a734dc5c211d14e47
KAILIN <2534992864@qq.com>
2019-08-28 20:32:40 +0800

    搜索功能

--- meiduo_mall/meiduo_mall/apps/goods/serializers.py
+++ meiduo_mall/meiduo_mall/apps/goods/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from drf_haystack.serializers import HaystackSerializer
 
 from .models import SKU
+from .search_indexes import SKUIndex
 
 class SKUSerializer(serializers.ModelSerializer):
     """
@@ -10,3 +12,12 @@ class SKUSerializer(serializers.ModelSerializer):
         model = SKU
         fields = ('id', 'name', 'price', 'default_image_url', 'comments')
 
+
+class SKUIndexSerializer(HaystackSerializer):
+    """
+    haystack使用的序列化器
+    """
+
+    class Meta:
+        index_classes = [SKUIndex]
+        fields = ('text', 'id', 'name', 'price', 'default_image_url', 'comments')
--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
@@ -8,3 +9,7 @@ urlpatterns = [
     url(r'^categories/(?P<category_id>\d+)/skus/$', views.SKUListView.as_view())
 ]
 
+router = DefaultRouter()
+router.register('skus/search', views.SKUSearchViewSet, base_name='skus-search')
+
+urlpatterns += router.urls
